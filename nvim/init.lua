32da7c61f04b98ee2e06b2c5a397341a564fcca5
Alexander Oxklint <alex.oxklint@gmail.com>
2023-12-03 16:07:18 +0100

    Move nvim conf to other repo and remove xps stuff

--- nvim/init.lua
+++
@@ -1,6 +0,0 @@
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.autocommands"
-require "user.colorscheme"
-require "user.gitsigns"
--- setup/laptop/setup_polybar_laptop.sh
+++
@@ -1,5 +0,0 @@
-sudo apt install polybar
-mkdir ~/.config/polybar
-ln -s ~/.dotfiles/polybar/config.xps13 ~/.config/polybar/config
-ln -s ~/.dotfiles/polybar/launch.sh ~/.config/polybar/launch.sh
-ln -s ~/.dotfiles/polybar/spotify_status.py ~/.config/polybar/spotify_status.py
